e64a494504e5fae7edd106a37b2dc7a6ec0686cf
Gabriela Caruso <gabriela.caruso@yahoo.com.br>
2020-11-30 19:38:04 -0300

    Faltando Client

--- script.py
+++ script.py
@@ -7,13 +7,14 @@
 import boto3
 import os
 import pprint
+import time
 pp = pprint.PrettyPrinter(indent=4)
 
 # =====================================================================================
 # GLOBAL CONSTANTS
 # =====================================================================================
-AWS_ACCESS_KEY_ID = "AKIAQV7Q65JNZLLCI5TS"
-AWS_SECRET_ACCESS_KEY = "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"
+AWS_ACCESS_KEY_ID = os.getenv("ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = os.getenv("SECRET_KEY")
 REGION_NAME = "us-east-2"  # Ohio
 REGION_NAME_2 = "us-east-1"  # North Virginia
 
@@ -37,6 +38,7 @@ ec2_nv = SESSION_NV.resource("ec2")
 client_oh = SESSION_OH.client("ec2")
 client_nv = SESSION_NV.client("ec2")
 client_lb = SESSION_NV.client("elb")
+client_lbv2 = SESSION_NV.client("elbv2")
 client_as = SESSION_NV.client("autoscaling")
 
 # Get VPC ids for both regions
@@ -47,41 +49,41 @@ VPC_NV = client_nv.describe_vpcs()["Vpcs"][0]["VpcId"]
 # FUNCTIONS
 # =====================================================================================
 
-# função para criar key pair
 
-# key_name = 'keyGabi'
+def key_pair(client, name):
+    print(f"[LOG] Deleting KeyPair '{name}'...")
+    client.delete_key_pair(KeyName=name)
+    print("[LOG] Deleted.")
 
-
-def key_pair(client, key_name):
-    client.delete_key_pair(KeyName=key_name)
-
-    key_pair = client.create_key_pair(KeyName=key_name)
-    key_name = "%s.pem" % key_name
+    print(f"[LOG] Creating KeyPair '{name}'...")
+    key_pair = client.create_key_pair(KeyName=name)
+    name = "%s.pem" % name
     try:
-        os.chmod(key_name, 0o777)
+        os.chmod(name, 0o777)
     except:
         pass
 
-    with open(key_name, "w") as text_file:
+    with open(name, "w") as text_file:
         text_file.write(key_pair["KeyMaterial"])
 
-    os.chmod(key_name, 0o400)
+    os.chmod(name, 0o400)
+    print("[LOG] Created.")
 
 
 def create_security_group(client, ec2, vpc_id, name):
-    print(f"[LOG] Creating SG '{name}'...")
+    print(f"[LOG] Creating SecurityGroup '{name}'...")
     try:
-        ec2.create_security_group(
-            Description="security group",
+        response = ec2.create_security_group(
+            Description="SecurityGroup",
             GroupName=name,
             VpcId=vpc_id,  # passar o describe pra ca e usar para o script inteiro
         )
-        print(f"[LOG] Created.")
+        print("[LOG] Created.")
     except Exception as e:
-        print(f"[LOG] Could not create SG '{name}'. Error: {e}")
+        print(f"[LOG] Could not create SecurityGroup '{name}'. ERROR: {e}.")
         return
 
-    print(f"[LOG] Creating SG rules...")
+    print("[LOG] Creating SecurityGroup rules...")
     client.authorize_security_group_ingress(
         GroupName=name,
         IpPermissions=[
@@ -133,16 +135,17 @@ def create_security_group(client, ec2, vpc_id, name):
         ],
     )
 
-    print(f"[LOG] Created.")
+    print("[LOG] Created.")
+    return response.group_id
 
 
 def delete_security_group(client, name):
-    print(f"[LOG] Deleting SG '{name}'")
+    print(f"[LOG] Deleting SecurityGroup '{name}'")
     try:
         client.delete_security_group(GroupName=name)
-        print(f"[LOG] Deleted")
+        print("[LOG] Deleted.")
     except Exception as e:
-        print(f"[LOG] Could not delete. Error: {e}")
+        print(f"[LOG] Could not delete SecurityGroup '{name}'. ERROR: {e}.")
 
 
 def delete_instance(client, ec2, name):
@@ -156,15 +159,15 @@ def delete_instance(client, ec2, name):
         )
 
     print(
-        f"[LOG] Terminating instances with name '{name}'. IDs: {instance_ids}")
+        f"[LOG] Terminating instances with name '{name}'. IDs: {instance_ids}.")
     client.terminate_instances(
         InstanceIds=instance_ids,
     )
 
-    print(f"[LOG] Waiting...")
+    print("[LOG] Waiting...")
     waiter = client.get_waiter("instance_terminated")
     waiter.wait(InstanceIds=instance_ids)
-    print(f"[LOG] Done.")
+    print("[LOG] Done.")
 
 
 def get_instance_id(client, name):
@@ -212,7 +215,7 @@ def create_database_instance(client, ec2, name, db_sg_name, key_name):
     sudo systemctl restart postgresql
     """
 
-    print(f"[LOG] Creating DB instance...")
+    print(f"[LOG] Creating Database instance with name '{name}'...")
     instances = ec2.create_instances(
         ImageId="ami-0dd9f0e7df0f0a138",  # AMI Ubunto 18.04 para Ohio
         MinCount=1,
@@ -232,11 +235,11 @@ def create_database_instance(client, ec2, name, db_sg_name, key_name):
     )
 
     instance = instances[0]
-    print(f"[LOG] DB instance ID: {instance.instance_id}")
+    print(f"[LOG] Database instance ID: {instance.instance_id}.")
 
     waiter = client.get_waiter("instance_running")
     waiter.wait(InstanceIds=[instance.instance_id])
-    print(f"[LOG] DB instance is running now.")
+    print("[LOG] Database instance is now running.")
 
     response = client.describe_instances(
         InstanceIds=[
@@ -246,7 +249,7 @@ def create_database_instance(client, ec2, name, db_sg_name, key_name):
 
     if len(response["Reservations"]) > 0:
         instance_ip = response["Reservations"][0]["Instances"][0]["PublicIpAddress"]
-        print(f"[LOG] DB instance public IP: {instance_ip}")
+        print(f"[LOG] Database instance public IP: {instance_ip}")
         return instance_ip
     else:
         print("[LOG] Error getting DB IP address.")
@@ -263,7 +266,7 @@ def create_orm_instance(client, ec2, name, orm_sg_name, key_name, db_pip):
     sudo reboot
     """
 
-    print(f"[LOG] Creating ORM instance...")
+    print(f"[LOG] Creating ORM instance with name '{name}'...")
     instances = ec2.create_instances(
         ImageId="ami-00ddb0e5626798373",  # AMI Ubunto 18.04 para North Virginia
         MinCount=1,
@@ -283,11 +286,11 @@ def create_orm_instance(client, ec2, name, orm_sg_name, key_name, db_pip):
     )
 
     instance = instances[0]
-    print(f"[LOG] ORM instance ID: {instance.instance_id}")
+    print(f"[LOG] ORM instance ID: {instance.instance_id}.")
 
     waiter = client.get_waiter("instance_running")
     waiter.wait(InstanceIds=[instance.instance_id])
-    print(f"[LOG] ORM instance is running now.")
+    print(f"[LOG] ORM instance is now running.")
 
     response = client.describe_instances(
         InstanceIds=[
@@ -297,65 +300,29 @@ def create_orm_instance(client, ec2, name, orm_sg_name, key_name, db_pip):
 
     if len(response["Reservations"]) > 0:
         instance_id = response["Reservations"][0]["Instances"][0]["InstanceId"]
-        print(f"[LOG] DB instance public ID: {instance_id}")
+        print(f"[LOG] DB instance public ID: {instance_id}.")
         return instance_id
     else:
         print("[LOG] Error getting DB IP address.")
 
 
-def create_ami(client, name, orm_id):
-    print(f"[LOG] Creating ORM image...")
-    client.create_image(
-        InstanceId=orm_id,
-        Name=name
-    )
-    print(f"[LOG] Created.")
+def delete_load_balancer(client_lb, client_lbv2, name):
+    print(f"[LOG] Deleting LoadBalancer '{name}'...")
+    try:
+        client_lb.delete_load_balancer(
+            LoadBalancerName=name
+        )
 
+        waiter = client_lbv2.get_waiter('load_balancers_deleted')
+        waiter.wait(Names=[name])
 
-# def deregister_ami(client, name, orm_id):
-#     pp = pprint.PrettyPrinter(indent=4)
-#     response = client.describe_images(
-#         Filters=[
-#             {
-#                 'Name': 'tag:Name',
-#                 'Values': [
-#                     name,
-#                 ],
-#             },
-#         ],
-#     )
-#     pp.pprint(response["Images"])
-
-    # response = client.deregister_image(
-    #     ImageId='string',
-    # )
-
-    # if len(response["Images"]) > 0:
-    #     print("[LOG] ORM image already created.")
-    # else:
-    #     print(f"[LOG] Creating ORM image...")
-    #     client.create_image(
-    #         InstanceId=orm_id,
-    #         Name=name
-    #     )
-    #     print(f"[LOG] Created.")
-
-def delete_load_balancer(client_lb, name):
-    print(f"[LOG] Deleting LB...")
-
-    response = client_lb.describe_load_balancers()
-    for i in response['LoadBalancerDescriptions']:
-        if i['LoadBalancerName'] == name:
-            client_lb.delete_load_balancer(
-                LoadBalancerName=name
-            )
-            print("[LOG] LB deleted.")
-        else:
-            print("[LOG] LB not found.")
+        print("[LOG] Deleted.")
+    except Exception as e:
+        print(f"[LOG] LoadBalancer '{name}' not found. ERROR: {e}.")
 
 
 def create_load_balancer(client, client_lb, name, sg_name):
-    print(f"[LOG] Creating Load Balancer...")
+    print(f"[LOG] Creating LoadBalancer with name '{name}'...")
 
     sg_id = client.describe_security_groups(
         GroupNames=[sg_name]
@@ -387,19 +354,45 @@ def create_load_balancer(client, client_lb, name, sg_name):
             },
         ]
     )
-    # waiter = client.get_waiter("load_balancer_exists")
-    # waiter.wait(InstanceIds=[instance.instance_id])
-    # print(f"[LOG] LB is running now.")
     print(f"[LOG] Created.")
 
 
-def create_auto_scaling_group(client_as, name, lb_name, orm_id):
-    print(f"[LOG] Creating Auto Scaling Group...")
+def delete_auto_scaling_group(client_as, name):
+    print(f"[LOG] Deleting AutoScalingGroup '{name}'...")
+    try:
+        client_as.delete_auto_scaling_group(
+            AutoScalingGroupName=name,
+            ForceDelete=True
+        )
+        response = client_as.describe_auto_scaling_groups(
+            AutoScalingGroupNames=[
+                name,
+            ]
+        )
+
+        while len(response["AutoScalingGroups"]) > 0:
+            print(
+                f"[LOG] Waiting for AutoScalingGroup '{name}' to be deleted.")
+            time.sleep(8)
+            response = client_as.describe_auto_scaling_groups(
+                AutoScalingGroupNames=[
+                    name,
+                ],
+            )
+
+        print("[LOG] Deleted.")
+    except Exception as e:
+        print(f"[LOG] AutoScalingGroup '{name}' not found. ERROR: {e}.")
+
+
+def create_auto_scaling_group(client, client_as, name, lc_name, lb_name):
+    print(f"[LOG] Creating AutoScalingGroup with name '{name}'...")
+
     client_as.create_auto_scaling_group(
         AutoScalingGroupName=name,
         HealthCheckGracePeriod=120,
         HealthCheckType='ELB',
-        InstanceId=orm_id,
+        LaunchConfigurationName=lc_name,
         LoadBalancerNames=[
             lb_name,
         ],
@@ -410,9 +403,52 @@ def create_auto_scaling_group(client_as, name, lb_name, orm_id):
                 'Key': 'Name',
                 'Value': 'ASGabi'
             },
-        ]
+        ],
+        AvailabilityZones=["us-east-1a", "us-east-1b"]
     )
-    print(f"[LOG] Created.")
+    print("[LOG] Created.")
+
+
+def delete_launch_configuration(client_as, name):
+    print(f"[LOG] Deleting LaunchConfiguration '{name}'...")
+    try:
+        client_as.delete_launch_configuration(
+            LaunchConfigurationName=name
+        )
+        print("[LOG] Deleted.")
+    except Exception as e:
+        print(f"[LOG] LaunchConfiguration '{name}' not found. ERROR: {e}.")
+
+
+def create_launch_configuration(client, name, sg, kp, db_pip):
+    print(f"[LOG] Creating LaunchConfiguration with name '{name}'...")
+
+    userdata = f"""#!/bin/sh
+    cd /home/ubuntu
+    sudo apt update
+    git clone https://github.com/gabicaruso/tasks.git
+    cd tasks
+    sudo sed -i 's/XXXX/{db_pip}/g' /home/ubuntu/tasks/portfolio/settings.py
+    ./install.sh
+    sudo reboot
+    """
+
+    try:
+        client.create_launch_configuration(
+            LaunchConfigurationName=name,
+            ImageId="ami-00ddb0e5626798373",  # AMI Ubunto 18.04 para North Virginia
+            KeyName=kp,
+            SecurityGroups=[
+                sg,
+            ],
+            UserData=userdata,
+            InstanceType='t2.micro',
+            AssociatePublicIpAddress=True,
+        )
+        print("[LOG] Created.")
+    except Exception as e:
+        print(
+            f"[LOG] Could not create LaunchConfiguration '{name}'. ERROR: {e}.")
 
 
 if __name__ == "__main__":
@@ -425,6 +461,7 @@ if __name__ == "__main__":
     orm_ami_name = "GABI_ORM_AMI"
     orm_lb_name = "GabiOrmLB"
     orm_as_name = "GABI_ORM_AS"
+    orm_lc_name = "GABI_ORM_LC"
 
     # Create key pair
     key_pair(client_oh, kp_name_oh)
@@ -432,31 +469,35 @@ if __name__ == "__main__":
 
     # Delete instances
     delete_instance(client_oh, ec2_oh, db_i_name)
-    delete_instance(client_nv, ec2_nv, orm_i_name)
+
+    # Delete LB
+    delete_load_balancer(client_lb, client_lbv2, orm_lb_name)
+
+    # Delete AS
+    delete_auto_scaling_group(client_as, orm_as_name)
+
+    # Delete LC
+    delete_launch_configuration(client_as, orm_lc_name)
 
     # Delete SGs
     delete_security_group(client_oh, db_sg_name)
     delete_security_group(client_nv, orm_sg_name)
 
-    # Delete LB
-    delete_load_balancer(client_lb, orm_lb_name)
-
     # Create SGs
     create_security_group(client_oh, ec2_oh, VPC_OH, db_sg_name)
-    create_security_group(client_nv, ec2_nv, VPC_NV, orm_sg_name)
+    orm_sg_id = create_security_group(client_nv, ec2_nv, VPC_NV, orm_sg_name)
 
     # Create instances
     db_pip = create_database_instance(
         client_oh, ec2_oh, db_i_name, db_sg_name, kp_name_oh)
-    orm_id = create_orm_instance(
-        client_nv, ec2_nv, orm_i_name, orm_sg_name, kp_name_nv, db_pip)
 
-    # Create image
-    # deregister_ami(client_nv, orm_ami_name, 'i-00ec7c9c6fca07916')
-    # create_ami(client_nv, orm_ami_name, 'i-00ec7c9c6fca07916')
+    # Create LC
+    create_launch_configuration(
+        client_as, orm_lc_name, orm_sg_id, kp_name_nv, db_pip)
 
     # Create LB
     create_load_balancer(client_nv, client_lb, orm_lb_name, orm_sg_name)
 
     # Create AS
-    create_auto_scaling_group(client_as, orm_as_name, orm_lb_name, orm_id)
+    create_auto_scaling_group(client_nv, client_as,
+                              orm_as_name, orm_lc_name, orm_lb_name)
